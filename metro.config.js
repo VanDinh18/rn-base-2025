4d888fae0b87f70ef1e0254ad389bed9df264aac
Nguyễn Đình <nguyendinh@MacBook-Pro-cua-Nguyen.local>
2025-04-02 11:19:01 +0700

    add tailwind config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,25 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfigs = getDefaultConfig(__dirname, { isCSSEnabled: true });
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const { assetExts, sourceExts } = defaultConfigs.resolver;
+
+let configs = mergeConfig(defaultConfigs, {
+  transformer: {
+    babelTransformerPath: require.resolve(
+        'react-native-svg-transformer/react-native'
+      ),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+});
+
+configs = wrapWithReanimatedMetroConfig(configs);
+
+module.exports = withNativeWind(configs, { input: './global.css' });
